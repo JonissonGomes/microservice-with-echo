8245aff0a25628289e04d6bfb8343eb820012b6d
jonisson.gomes <jonisson.gomes@g.globo>
2022-07-04 17:12:42 -0300

    Refactoring main archive

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-
+	"github.com/JonissonGomes/microservice-with-echo/handlers"
 	"github.com/labstack/echo"
 )
 
@@ -13,57 +9,6 @@ func main() {
 	echo := echo.New()
 	echo.Logger.Fatal(echo.Start(":9000"))
 
-	echo.GET("/product/:data", GetProduct)
-	echo.POST("/product", AddProduct)
-}
-
-func GetProduct(c echo.Context) error {
-	prodName := c.QueryParam("name")
-	prodType := c.QueryParam("type")
-	prodCategory := c.QueryParam("category")
-	prodSubCategory := c.QueryParam("subcategory")
-	prodPrice := c.QueryParam("price")
-	dataType := c.Param("datatype")
-
-	if dataType == "string" {
-		return c.String(http.StatusOK, fmt.Sprintf("O produto %s chegou no formato %s", prodName, dataType))
-	}
-
-	if dataType == "json" {
-		return c.JSON(http.StatusOK, map[string]string{
-			"name":        prodName,
-			"type":        prodType,
-			"category":    prodCategory,
-			"subcategory": prodSubCategory,
-			"price":       prodPrice,
-			"datatype":    dataType,
-		})
-	} else {
-		return c.JSON(http.StatusBadRequest, map[string]string{
-			"error": "Verifique se o tipo está no formado String ou JSON"})
-	}
-}
-
-func AddProduct(c echo.Context) error {
-	type Product struct {
-		Name        string `json:"name"`
-		Type        string `json:"type"`
-		Category    string `json:"category"`
-		SubCategory string `json:"subCategory"`
-		Price       string `json:"price"`
-		DataType    string `json:"datatype"`
-	}
-
-	newProduct := Product{}
-
-	defer c.Request().Body.Close()
-
-	err := json.NewDecoder(c.Request().Body).Decode(&newProduct)
-
-	if err != nil {
-		log.Fatal("Não foi possível ler requisição")
-		return echo.NewHTTPError(http.StatusInternalServerError, err.Error)
-	}
-
-	return c.String(http.StatusOK, "Produto adicionando!")
+	echo.GET("/product/:data", handlers.GetProduct)
+	echo.POST("/product", handlers.AddProduct)
 }
